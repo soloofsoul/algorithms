527811f5dc31370092df68d839dacc98325b75f5
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2021-03-12 08:49:30 +0100

    Google: added solution for calculation volume of lakes

---
+++ Google interview/volume_of_lakes.js
@@ -0,0 +1,36 @@
+/*
+Imagine an island that is in the shape of a bar graph. When it rains, certain areas of the island fill up with rainwater to form lakes. Any excess rainwater the island cannot hold in lakes will run off the island to the west or east and drain into the ocean.
+
+Given an array of positive integers representing 2-D bar heights, design an algorithm (or write a function) that can compute the total volume (capacity) of water that could be held in all lakes on such an island given an array of the heights of the bars. Assume an elevation map where the width of each bar is 1.
+
+Example: Given [1,3,2,4,1,3,1,4,5,2,2,1,4,2,2], return 15 (3 bodies of water with volumes of 1,7,7 yields total volume of 15)
+
+Learning objectives
+This question offers practice with algorithms, data structures, Big-O, defining functions, generalization, efficiency, time and space complexity, and anticipating edge cases.
+ */
+
+const vol = arr => {
+  let trap = 0;
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+
+  while (leftIndex < rightIndex) {
+    if (arr[leftIndex] <= arr[rightIndex]) {
+      leftMax = Math.max(leftMax, arr[leftIndex]);
+      trap += Math.max(0, leftMax - arr[leftIndex])
+      leftIndex++;
+    } else {
+      rightMax = Math.max(rightMax, arr[rightIndex]);
+      trap += Math.max(0, rightMax - arr[rightIndex])
+      rightIndex--;
+    }
+  }
+
+  return trap;
+};
+
+console.log(vol( [1,3,2,4,1,3,1,4,5,2,2,1,4,2,2]));
+
+// Dijkstra algorithm is used to find volume
