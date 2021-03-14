ecdabd1fa22864db70a0e61ab522e87cc6106d76
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2021-03-14 21:35:15 +0100

    Added stack array-implementation

--- Others/Data structures/stack.js
+++ Others/Data structures/stack.js
@@ -1,3 +1,4 @@
+// Linked list implementation
 const list = {
   first: null,
   isEmpty: function isEmpty() {
@@ -18,10 +19,36 @@ const list = {
 };
 
 console.log(list);
-list.push('item 1')
-list.push('item 2')
+list.push('item 1');
+list.push('item 2');
 console.log(list);
 console.log(list.pop());
 list.pop();
 console.log(list);
 console.log(list.isEmpty());
+
+// array implementation
+const list1 = {
+  string: [],
+  ind: 0,
+  isEmpty: function isEmpty() {
+    return this.ind === 0;
+  },
+  push: function push(item) {
+    this.string[this.ind++] = item;
+  },
+  pop: function pop() {
+    const item = this.string[--this.ind];
+    this.string[this.ind] = null;
+    return item;
+  }
+};
+
+console.log(list1.string);
+list1.push('item 1');
+list1.push('item 2');
+console.log(list1.string);
+console.log(list1.pop());
+list1.pop();
+console.log(list1.string);
+console.log(list1.isEmpty());
