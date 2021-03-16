d04440e8464ca2bd98c075eed2ed4074b9a00b51
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2021-03-16 12:34:42 +0100

    Added shell sort solution

---
+++ Algorithms/Sort/shell_sort.js
@@ -0,0 +1,30 @@
+// Shell sort with Sendgewick increment sequence 3x+1
+
+function swapItems(arr, p, q) {
+  const swap = arr[p];
+  arr[p] = arr[q];
+  arr[q] = swap;
+}
+
+function shellSort(arr) {
+  const n = arr.length;
+  let h = 1, i, j;
+
+  while (h < Math.floor(n / 3)) {
+    h = 3*h + 1;
+  }
+
+  while (h >= 1) {
+    for (i = h; i < n; i++) {
+      for(j = i; j >= h && arr[j] < arr[j-h]; j=j-h) {
+        swapItems(arr, j, j-h);
+      }
+    }
+
+    h = Math.floor(h/3);
+  }
+}
+
+const arr = [2,1,5,3,8,6,15,-1,52,0,10,16,7,11,65];
+shellSort(arr);
+console.log(arr);
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 Problems solving using Javascript
 
-* [toLowerCase.js](toLowerCase.js)
+* [toLowerCase.js](Others/toLowerCase.js)
 * Facebook interview
+* Google interview
+* Data structures
+* Algorithms
 * Leetcode
-
+* Other tasks solutions
 
